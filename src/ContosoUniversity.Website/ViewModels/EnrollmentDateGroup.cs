fb1c232eb576a4923273d318a30d9742cc03fabf
simonray <simonray@hotmail.com>
2015-09-15 09:08:34 +0100

    Apply date formatting

--- src/ContosoUniversity.Website/ViewModels/EnrollmentDateGroup.cs
+++ src/ContosoUniversity.Website/ViewModels/EnrollmentDateGroup.cs
@@ -6,6 +6,7 @@ namespace ContosoUniversity.ViewModels
     public class EnrollmentDateGroup
     {
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? EnrollmentDate { get; set; }
         public int StudentCount { get; set; }
     }
